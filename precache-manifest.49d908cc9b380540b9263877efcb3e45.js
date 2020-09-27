119422dcfc690f1b72c02e9f0c755eeca1ebe439
qiuhangwei <1239197997@qq.com>
2020-09-27 11:43:20 +0800

    build

---
+++ precache-manifest.49d908cc9b380540b9263877efcb3e45.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e966e7f3ddc336612e4ba800d69a655",
+    "url": "/github-deploy-demo/index.html"
+  },
+  {
+    "revision": "2acca2dc29e8ab77993d",
+    "url": "/github-deploy-demo/static/css/2.1977312c.chunk.css"
+  },
+  {
+    "revision": "1db9636519242624a167",
+    "url": "/github-deploy-demo/static/css/main.47416c6d.chunk.css"
+  },
+  {
+    "revision": "2acca2dc29e8ab77993d",
+    "url": "/github-deploy-demo/static/js/2.e6588d17.chunk.js"
+  },
+  {
+    "revision": "9d226f5d128ebd9399b83c1a0d67574c",
+    "url": "/github-deploy-demo/static/js/2.e6588d17.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1db9636519242624a167",
+    "url": "/github-deploy-demo/static/js/main.2eed1af0.chunk.js"
+  },
+  {
+    "revision": "a3a4993f9c47d3746832",
+    "url": "/github-deploy-demo/static/js/runtime-main.8a48077b.js"
+  },
+  {
+    "revision": "b1147e934c767a49ddd6a9256c2bcf9f",
+    "url": "/github-deploy-demo/static/media/Ezreal.b1147e93.png"
+  },
+  {
+    "revision": "70e7345588a3ebc17870b7319907446a",
+    "url": "/github-deploy-demo/static/media/Galio.70e73455.png"
+  },
+  {
+    "revision": "9322fae947c9b05719293b07b62e6dc5",
+    "url": "/github-deploy-demo/static/media/Olaf.9322fae9.png"
+  },
+  {
+    "revision": "b91c10bcdddc3ff1aa4239ed41deffcc",
+    "url": "/github-deploy-demo/static/media/Ornn.b91c10bc.png"
+  },
+  {
+    "revision": "6377e1dd0eb67efe4a43078d570402ae",
+    "url": "/github-deploy-demo/static/media/Thresh.6377e1dd.png"
+  },
+  {
+    "revision": "bc5381bd91f107d959e2ce9cbc15c098",
+    "url": "/github-deploy-demo/static/media/ao.bc5381bd.png"
+  },
+  {
+    "revision": "d28252fe7eb33e3d2ecf7c1165f42826",
+    "url": "/github-deploy-demo/static/media/book1.d28252fe.png"
+  },
+  {
+    "revision": "4cde60e21acd70e4ab12e95f0bae4099",
+    "url": "/github-deploy-demo/static/media/book2.4cde60e2.png"
+  },
+  {
+    "revision": "130927f87cd4b3105c23bd843c645ae2",
+    "url": "/github-deploy-demo/static/media/book3.130927f8.png"
+  },
+  {
+    "revision": "15f7fd71a6d01f95cc690d98cde8dc7f",
+    "url": "/github-deploy-demo/static/media/book4.15f7fd71.png"
+  },
+  {
+    "revision": "3483b48359344619614d37b119ce3c5b",
+    "url": "/github-deploy-demo/static/media/cap.3483b483.png"
+  },
+  {
+    "revision": "9cb86c4d6a27b47ea1ddb9fa0e8f3b65",
+    "url": "/github-deploy-demo/static/media/code.9cb86c4d.png"
+  },
+  {
+    "revision": "d1dbbd0a2b1382634036f31d4b8640c9",
+    "url": "/github-deploy-demo/static/media/code2.d1dbbd0a.png"
+  },
+  {
+    "revision": "c810665d880c76dc5a5dc6b4e98cb963",
+    "url": "/github-deploy-demo/static/media/film1.c810665d.png"
+  },
+  {
+    "revision": "87c331d66f182591787db47131dc12ea",
+    "url": "/github-deploy-demo/static/media/film2.87c331d6.png"
+  },
+  {
+    "revision": "8683c2b4f036e110d9378d6529ed8e0c",
+    "url": "/github-deploy-demo/static/media/film3.8683c2b4.png"
+  },
+  {
+    "revision": "15285fbb47a540c2fc6654e3a4e5b9d3",
+    "url": "/github-deploy-demo/static/media/film4.15285fbb.png"
+  },
+  {
+    "revision": "491ed44f9095a9f9e870fd0ab87401cf",
+    "url": "/github-deploy-demo/static/media/game1.491ed44f.png"
+  },
+  {
+    "revision": "77bd0e6f79a38f501475ca8881412915",
+    "url": "/github-deploy-demo/static/media/game2.77bd0e6f.png"
+  },
+  {
+    "revision": "43ee8cc01685cb2828c40fa4d007de56",
+    "url": "/github-deploy-demo/static/media/game3.43ee8cc0.png"
+  },
+  {
+    "revision": "62592aad6cb464c9989fa97ee93c9eae",
+    "url": "/github-deploy-demo/static/media/latte.62592aad.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-deploy-demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "ad6c5d8de5af009ddeeec335a31d49f3",
+    "url": "/github-deploy-demo/static/media/logo.ad6c5d8d.png"
+  },
+  {
+    "revision": "ebc3ec00f8aeb2caf63f5cae7fd87c13",
+    "url": "/github-deploy-demo/static/media/news.ebc3ec00.png"
+  },
+  {
+    "revision": "6f3fa1ddea0afcd59217327818dec5e2",
+    "url": "/github-deploy-demo/static/media/title-bar.6f3fa1dd.png"
+  }
+]);
